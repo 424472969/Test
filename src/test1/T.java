4ea33419615d4bc113252bffa93304885c5ba149
LIN-25\Lin <424472969@qq.com>
2017-02-22 16:37:45 +0800

    111

--- src/test1/T.java
+++ src/test1/T.java
@@ -1,7 +1,30 @@
 package test1;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class T {
-public static void main(String[] args) {
-	
+	public static void main(String args[]) throws java.io.IOException {
+			TimerTask task = new TimerTask() {
+				public void run() {
+					System.out.print("Hi");
+				}
+			};
+			Timer timer = new Timer();
+			timer.schedule(task, 0, 1000);
+			System.out.println("Press ENTER to stop");
+			System.in.read(new byte[10]);
+			timer.cancel();
+	}
 }
-}
+
+
+
+
+
+
+
+
+
+
+
